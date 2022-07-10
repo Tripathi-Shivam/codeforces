89fd25ddf8fab0e1174572b48fa4fe909dd61e79
Tripathi-Shivam <tripathishivam11@gmail.com>
2022-07-10 18:23:39 +0530

    a20j_ladder_11 Problem 53

---
+++ a2oj_ladder_11/Problem_118A_StringTask.java
@@ -0,0 +1,81 @@
+package a2oj_ladder_11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Problem_118A_StringTask {
+
+	public static FastScanner fs = new FastScanner();
+    public static PrintWriter out = new PrintWriter(System.out);
+	
+	public static void main(String[] args) {
+        out.println(solve(fs.next()));
+        out.close();
+	}
+
+	public static String solve(String s) {
+		StringBuilder result = new StringBuilder();
+		int n = s.length();
+		s = s.toLowerCase();
+		
+		for(int i = 0; i < n; i++) {
+			if(s.charAt(i) != 'a' && s.charAt(i) != 'e' && s.charAt(i) != 'i' && s.charAt(i) != 'o' && s.charAt(i) != 'u' && s.charAt(i) != 'y')
+				result.append("." + s.charAt(i));
+		}
+		
+		return result.toString();
+	}
+	
+	static void sort(int[] a) {
+		ArrayList<Integer> l=new ArrayList<>();
+		for (int i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
+	
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+		
+		String next() {
+			while(!st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i<n; i++) a[i]=nextInt();
+            return a;
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+	}
+	
+	public static String toString(char[] a) {
+        StringBuilder sb = new StringBuilder();
+ 
+        // Creating a string using append() method
+        for (int i = 0; i < a.length; i++) {
+            sb.append(a[i]);
+        }
+ 
+        return sb.toString();
+    }
+}
